666b92628414b53068d4315a5e242a3acc6eb5f6
ZhenyuWu323 <zhw323@lehigh.edu>
2023-05-30 01:11:37 -0400

    create spring-boot project(executable)

--- SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/SpringBootTutorialApplication.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/SpringBootTutorialApplication.java
@@ -1,4 +1,4 @@
-package CSE298.SpringBoot.tutorial;
+package CSE298.SpringBoot_tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SpringBoot_tutorial/SpringBoot_tutorial/src/test/java/CSE298/SpringBoot_tutorial/SpringBootTutorialApplicationTests.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/test/java/CSE298/SpringBoot_tutorial/SpringBootTutorialApplicationTests.java
@@ -1,4 +1,4 @@
-package CSE298.SpringBoot.tutorial;
+package CSE298.SpringBoot_tutorial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
