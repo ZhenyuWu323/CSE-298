20865803d5492979e25714d4da373c523bed02d8
ZhenyuWu323 <zhw323@lehigh.edu>
2023-06-15 14:48:31 -0400

    modify loading effect

--- React_Tutorial/src/components/GameGrid.tsx
+++ React_Tutorial/src/components/GameGrid.tsx
@@ -48,18 +48,27 @@ function GameGrid() {
   return (
     <Box mx="auto" maxW="auto" p={4}>
       <>
-        <Grid templateColumns="repeat(4, 1fr)" gap={6} gridAutoFlow="row dense">
-          {gameList.map((game: Game) => (
-            <Skeleton isLoaded={!isLoading} key={game.ID} fadeDuration={1}>
+        {/* Game Cards */}
+        {isLoading ? (
+          <Grid templateColumns="repeat(4, 1fr)" gap={6} gridAutoFlow="row dense">
+            {Array.from({ length: 20 }).map((_, index) => (
+              <Skeleton key={index} height="300px" width="400px"/>
+            ))}
+          </Grid>
+        ) : (
+          <Grid templateColumns="repeat(4, 1fr)" gap={6} gridAutoFlow="row dense">
+            {gameList.map((game: Game) => (
               <Card key={game.ID} borderRadius={10} overflow="hidden">
                 <Image src={ResizeImage(game.Image)} />
                 <CardBody>
                   <Heading fontSize="2xl">{game.Name}</Heading>
                 </CardBody>
               </Card>
-            </Skeleton>
-          ))}
-        </Grid>
+            ))}
+          </Grid>
+        )}
+        
+        {/* Page Bar */}
         <Skeleton isLoaded={!isLoading} key="PageBar">
           <Flex justify="center" alignItems="center" mt={4}>
             {pageNum > 1 && (
