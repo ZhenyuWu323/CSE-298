8d6fd25c071ba480cccd211bd37fb7ad8108baf5
ZhenyuWu323 <zhw323@lehigh.edu>
2023-06-23 07:37:30 -0400

    restruct response

--- SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/controller/GameApiController.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/controller/GameApiController.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @Validated
@@ -36,7 +37,7 @@ public class GameApiController {
     }
 
     @GetMapping("/games")
-    public Object getGameList(@RequestParam("page") @Min(1) int PageNum) throws ParseException {
+    public Object getGameList(@RequestParam("page") @Min(1) @Max(50) int PageNum) throws ParseException {
         GameList.CleanGame();
             String steamApiUrl = "https://api.rawg.io/api/games?key=c5cbbf661c79425e9064cd8c3976db42&page=" + String.valueOf(PageNum);
             System.out.println(steamApiUrl);
@@ -64,16 +65,64 @@ public class GameApiController {
             //Parse the string response
             JSONParser parser = new JSONParser();
             JSONObject res = (JSONObject) parser.parse(String.valueOf(response));
+
             Integer totalPage = Integer.parseInt(res.get("count").toString()) / 20;
+            
             JSONArray game_list = (JSONArray) res.get("results");
             System.out.println(game_list.size());
             for (int i = 0; i < game_list.size(); i++){
-            JSONObject game = (JSONObject) game_list.get(i);
-            String name = game.get("name").toString();
-            Integer id = Integer.parseInt(game.get("id").toString());
-            String image = game.get("background_image").toString();
-            Games temp = new Games(id, name,image);
-            GameList.AddGame(temp);
+                //Each Game
+                JSONObject game = (JSONObject) game_list.get(i);
+
+                //Game Name
+                String name = game.get("name").toString();
+
+                //Game ID
+                Integer id = Integer.parseInt(game.get("id").toString());
+
+                //Game Image
+                String image = game.get("background_image").toString();
+
+                //Game Genre
+                JSONArray genre_list = (JSONArray) game.get("genres");
+                List<String> genre = new ArrayList<>();
+                for(int j = 0; j < genre_list.size(); j++){
+                    JSONObject temp = (JSONObject) genre_list.get(j);
+                    String item = temp.get("name").toString();
+                    genre.add(item);
+                }
+
+                //Game Platform
+                JSONArray platform_list = (JSONArray) game.get("parent_platforms");
+                List<String> platform = new ArrayList<>();
+                for(int j = 0; j < platform_list.size(); j++){
+                    JSONObject temp = (JSONObject) platform_list.get(j);
+                    JSONObject each_temp = (JSONObject) temp.get("platform");
+                    String item = each_temp.get("name").toString();
+                    platform.add(item);
+                }
+
+                //Game metacritic
+                String metacritic = "";
+                if (game.get("metacritic") == null){
+                    metacritic = "0";
+                }
+                else{
+                    metacritic = game.get("metacritic").toString();
+                }
+
+
+                //Game release
+                String release = "";
+                if (game.get("released") == null){
+                    release = "0";
+                }
+                else{
+                    release = game.get("released").toString();
+                }
+
+                Games singleGame = new Games(id, name,image, genre, platform, metacritic, release);
+                GameList.AddGame(singleGame);
             }
             return new GameContent(totalPage, GameList.FindAllGames());
         } 
--- SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/model/Games.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/model/Games.java
@@ -1,8 +1,15 @@
 package CSE298.SpringBoot_tutorial.model;
+
+import java.util.List;
+
 public record Games(
     Integer ID,
     String Name,
-    String Image
+    String Image,
+    List<String> Genre,
+    List<String> Platform,
+    String Metacritic,
+    String Released
 ){
 
 }
