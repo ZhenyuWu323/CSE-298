47dc8caa0015b785afffe55eb89d5b63367491c8
ZhenyuWu323 <zhw323@lehigh.edu>
2023-07-27 04:37:35 -0400

    oauth

---
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/config/Security.java
@@ -0,0 +1,32 @@
+package CSE298.SpringBoot_tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+    
+    @Bean
+    SecurityFilterChain filetChain(HttpSecurity http) throws Exception{
+        return http
+        .authorizeHttpRequests(
+            auth->{
+                auth.requestMatchers("/api/**").permitAll();
+                auth.anyRequest().authenticated();
+            }
+        )
+        .oauth2Login(null)
+        .build();
+    
+    }
+
+    
+
+    
+}
