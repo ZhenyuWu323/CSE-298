74004b8526e5947082376cd9cf1ab8c2d46ada68
ZhenyuWu323 <zhw323@lehigh.edu>
2023-07-28 05:32:33 -0400

    change return type

--- SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/controller/UserController.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/controller/UserController.java
@@ -2,8 +2,10 @@ package CSE298.SpringBoot_tutorial.controller;
 
 import java.util.Map;
 
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,20 +56,15 @@ public class UserController {
 
     //HomeRun Signup
     @PostMapping("/homesign")
-    public UserProfile HomeSignUp(@RequestParam("name") String name, @RequestParam("pass") String pass) {
+    public ResponseEntity<String> HomeSignUp(@RequestParam("name") String name, @RequestParam("pass") String pass) {
         // Check if a UserProfile with the given name already exists in the database
         UserProfile existingUser = UserRepo.findByName(name);
         if (existingUser != null) {
-            // If the user already exists, return an error response to the frontend
-            //return ResponseEntity.status(HttpStatus.SC_CONFLICT).body("Duplicate User Name\n");
-            return null;
+            return ResponseEntity.status(HttpStatus.SC_CONFLICT).body("Duplicate User Name\n");
         } else {
-            // If the user doesn't exist, create a new UserProfile and save it in the database
             UserProfile newUser = new UserProfile(name, pass, "none");
             UserProfile savedUser = UserRepo.save(newUser);
-            // You can perform other actions or return a success response if needed.
-            //return ResponseEntity.status(HttpStatus.SC_CREATED).body("New User Signed Up\n");
-            return savedUser;
+            return ResponseEntity.status(HttpStatus.SC_CREATED).body("New User Signed Up\n");
         }
     }
 }
