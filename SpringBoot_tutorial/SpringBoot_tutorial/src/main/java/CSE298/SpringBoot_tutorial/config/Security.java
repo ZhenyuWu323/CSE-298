1b927b931c3810e39a532f4a81fb497ed3d0146d
ZhenyuWu323 <zhw323@lehigh.edu>
2023-08-02 14:01:34 -0400

    logout

--- SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/config/Security.java
+++ SpringBoot_tutorial/SpringBoot_tutorial/src/main/java/CSE298/SpringBoot_tutorial/config/Security.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.cli
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -34,7 +37,7 @@ public class Security {
         .oauth2Login(login->login.defaultSuccessUrl("/", true))
         .logout(logoutConfigurer -> {
             logoutConfigurer
-                .logoutUrl("/logout")
+                .logoutRequestMatcher((RequestMatcher) new AntPathRequestMatcher("/logout", "GET"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
