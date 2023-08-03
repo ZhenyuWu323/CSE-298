0d039fbe9db4533b6eed86b9bef5af56cb014c0f
ZhenyuWu323 <zhw323@lehigh.edu>
2023-08-03 12:18:35 -0400

    updates

--- React_Tutorial/src/components/CommentList.tsx
+++ React_Tutorial/src/components/CommentList.tsx
@@ -5,6 +5,8 @@ import {
     Avatar,
     VStack,
     Button,
+    SkeletonCircle,
+    SkeletonText,
   } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Comments } from './CommentGrid';
@@ -20,7 +22,7 @@ interface Props{
 export const CommentList = ({ comments, isLoading }: Props) => {
     const[page, setPage] = useState(0);
     const currentPageComments = comments.slice(page * 10, (page + 1) * 10);
-    const totalPage = comments.length>10 ? comments.length/10 : 1
+    const totalPage = Math.ceil(comments.length / 10);
     // Make sure the list always has 10 elements
     const paddedComments = [...currentPageComments];
     while (paddedComments.length < 10) {
@@ -30,51 +32,64 @@ export const CommentList = ({ comments, isLoading }: Props) => {
     return (
         
         <VStack spacing={4} align="stretch">
-            {paddedComments.map((comment, i) => (
-                <Box key={comment ? comment.id : i} p={5} shadow="md" borderWidth="1px">
-                {comment ? (
-                    <>
-                    <Flex>
-                        <Avatar src={comment.userImage} size='sm'/>
-                        <Text ml={4}>{comment.userName}</Text>
-                    </Flex>
-                    <Text mt={2} align="left">
-                        {comment.content}
-                    </Text>
-                    </>
-                ) : (
-                    // Placeholder for empty comments
-                    <Text color="gray.500">No comment</Text>
-                )}
-                </Box>
-            ))}
-            <Flex justify="center" alignItems="center" mt={4}>
-                <Button
-                    leftIcon={<TbArrowBigLeft />}
-                    colorScheme="gray"
-                    variant="solid"
-                    onClick={() =>{
-                        if(page != 0){
-                            setPage(page-1)
-                        }
-                    }}
-                  >
-                    Back
-                </Button>
-                <Box mx={6}>Page {page+1 + " / " + totalPage}</Box>
-                <Button
-                    leftIcon={<TbArrowBigRight />}
-                    colorScheme="gray"
-                    variant="solid"
-                    onClick={() =>{
-                        if(page < totalPage){
-                            setPage(page+1)
-                        }
-                    }}
-                  >
-                    Next
-                </Button>
+        {isLoading ? (
+          Array(10).fill(0).map((_, i) => (
+            <Box key={i} p={5} shadow="md" borderWidth="1px">
+              <Flex>
+                <SkeletonCircle size='30px'/>
+                <SkeletonText ml={4} noOfLines={1} width='200px' />
               </Flex>
-        </VStack>
+              <SkeletonText mt={2} noOfLines={4} width='100%' />
+            </Box>
+          ))
+        ) : (
+          paddedComments.map((comment, i) => (
+            <Box key={comment ? comment.id : i} p={5} shadow="md" borderWidth="1px">
+              {comment ? (
+                <>
+                  <Flex>
+                    <Avatar src={comment.userImage} size='sm'/>
+                    <Text ml={4}>{comment.userName}</Text>
+                    <Text ml={4}>{comment.time}</Text>
+                  </Flex>
+                  <Text mt={2} align="left">
+                    {comment.content}
+                  </Text>
+                </>
+              ) : (
+                <Text color="gray.500">No comment</Text>
+              )}
+            </Box>
+          ))
+        )}
+        <Flex justify="center" alignItems="center" mt={4}>
+          <Button
+            leftIcon={<TbArrowBigLeft />}
+            colorScheme="gray"
+            variant="solid"
+            onClick={() => {
+              if (page !== 0) {
+                setPage(page - 1)
+              }
+            }}
+          >
+            Back
+          </Button>
+          <Box mx={6}>Page {page + 1 + " / " + totalPage}</Box>
+          <Button
+            rightIcon={<TbArrowBigRight />}
+            colorScheme="gray"
+            variant="solid"
+            onClick={() => {
+              if (page < totalPage - 1) {
+                setPage(page + 1)
+              }
+            }}
+          >
+            Next
+          </Button>
+        </Flex>
+      </VStack>
+  
     );
 };
